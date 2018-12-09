a76b8020e19c63c25f6044c19efb2f85b7c3d294
jianshuang_huang <jianshuang_huang@idgcapital.com>
2018-12-09 21:35:21 +0800

    upload

--- src/main/java/com/hjs/newcharter/Lambda1.java
+++ src/main/java/com/hjs/newcharter/Lambda1.java
@@ -1,20 +1,59 @@
 package com.hjs.newcharter;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * lambda表达式
+ * java8新特性
  */
 public class Lambda1 {
 
 
     public static void main(String[] args) {
 
+        Lambda1 tester = new Lambda1();
+
+        MathOperation addition = (int a, int b) -> a+b;
+        MathOperation subtraction = (a,b) -> a-b;
+        MathOperation multip = (int a, int b) -> {return a*b;};
+        MathOperation division = (a,b) -> a/b;
+
+        System.out.println("10+5=" + tester.operate(10,5,addition));
+
+        GreetingService service = message ->
+                System.out.println("Hello "+message);
+
+        GreetingService service1 = (message) -> {
+            System.out.println("Hello "+message);
+        };
+
+        service.sayMessage("HJS");
 
 
 
     }
 
-fg
-    public void
+    interface MathOperation {
+        int operation(int a, int b);
+    }
+
+    interface GreetingService{
+        void sayMessage(String message);
+    }
+
+
+    /**
+     * 处理a和b
+     * @param a
+     * @param b
+     * @param mathOperation
+     * @return
+     */
+    private int operate(int a, int b, MathOperation mathOperation){
+        return mathOperation.operation(a,b);
+    }
+
 
 }
--- src/main/java/com/hjs/newcharter/New1.java
+++ src/main/java/com/hjs/newcharter/New1.java
@@ -1,6 +1,9 @@
 package com.hjs.newcharter;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Java的新特性
  */
@@ -8,7 +11,16 @@ public class New1 {
 
     public static void main(String[] args) {
 
-        System.out.println("HJS");
+        List names = new ArrayList();
+
+        names.add("Google");
+        names.add("Runoob");
+        names.add("Taobao");
+        names.add("Baidu");
+        names.add("Sina");
+
+
+        names.forEach(System.out :: println);
 
     }
 
